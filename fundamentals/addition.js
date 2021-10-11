33ef1ed20057ebb6235fda18d57e47c0f36041e5
vamshi5791 <vamshigovindhu@gmail.com>
2021-10-11 18:29:37 +0530

    commit just

--- fundamentals/addition.js
+++ fundamentals/addition.js
@@ -2,6 +2,7 @@ const num1=4;
 const num2=5;
 const sum=num1+num2;
 console.log('addition of'+num1+'and'+num2+'is'+sum)
+console.log("hello")
 
 const num3=parseInt(prompt('Enter first number'));
 const num4=parseInt(prompt('Enter any second number'));
